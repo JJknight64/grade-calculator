1b4e9808ba59dc46538824f704a0154b1acf9490
JJknight64 <jwd8744@rit.edu>
2024-11-02 15:41:31 -0400

    Add project files.

---
+++ grade-calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Jacob Duryea
+IGME 201
+11/2/2024 (due 10/13/2024)
+*/
+
+namespace grade_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = "Jacob";
+            int[] grades = { 100, 92, 87, 200, -20, 52, 82, 75, 67, 88};
+            Console.WriteLine("Welcome, " + studentName + "!");
+
+        }
+    }
+}
