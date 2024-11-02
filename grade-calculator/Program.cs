1264ab1ae672b1b0ded00e4e085332a45dd4c902
JJknight64 <jwd8744@rit.edu>
2024-11-02 17:57:18 -0400

    forgot to keep committing stuff as i went along so here's the entire thing LOL

--- grade-calculator/Program.cs
+++ grade-calculator/Program.cs
@@ -11,9 +11,51 @@ namespace grade_calculator
         static void Main(string[] args)
         {
             string studentName = "Jacob";
-            int[] grades = { 100, 92, 87, 200, -20, 52, 82, 75, 67, 88};
+            int[] grades = {100, 92, 87, 200, -20, 52, 82, 75, 67, 88};
             Console.WriteLine("Welcome, " + studentName + "!");
+            Console.WriteLine("Here are your grades:");
+            int average = 0;
 
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine(grades[i]);
+                if (90 <= grades[i] && grades[i] <= 100)
+                {
+                    Console.WriteLine("This grade is an A!");
+                    if (grades[i] == 100)
+                    {
+                        Console.WriteLine("WOW!!! A perfect score!");
+                    }
+                }
+                else if (80 <= grades[i] && grades[i] <= 89)
+                {
+                    Console.WriteLine("This grade is a B!");
+                }
+                else if (70 <= grades[i] && grades[i] <= 79)
+                {
+                    Console.WriteLine("This grade is a C.");
+                }
+                else if (65 <= grades[i] && grades[i] <= 69)
+                {
+                    Console.WriteLine("This grade is a D.");
+                }
+                else if (0 <= grades[i] && grades[i] <= 64)
+                {
+                    Console.WriteLine("This grade is an F...");
+                }
+                else
+                {
+                    Console.WriteLine("This is out of the range of 0 - 100. How did you even get this grade...?");
+                }
+            }
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                average += grades[i];
+            }
+            average = average / grades.Length;
+            Console.WriteLine("Your final calculated average is: " + average);
+            Console.WriteLine("All grades have been displayed for " + studentName);
         }
     }
 }
